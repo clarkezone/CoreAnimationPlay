1497ceb0e4adb8fdc540ac24d660bf6feb48c859
james <james@clarkezone.net>
2018-05-31 17:11:11 -0700

    Initial Commit

---
+++ GraphicsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GraphicsTest
+//
+//  Created by James Clarke on 5/31/18.
+//  Copyright © 2018 James Clarke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
